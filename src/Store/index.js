27fc4464e6519528f0bc69703f88823e1111eca0
aziz <aziz@bunid.ca>
2021-04-19 15:49:19 -0400

    remove console log

--- src/Store/index.js
+++ src/Store/index.js
@@ -25,7 +25,6 @@ function decrement (counter) {
 export const reducer = (state = initialSate, action) => {
   switch (action.type) {
     case FETCH_PEOPLE:
-      console.log(action);
       return {
         ...state,
         loading: false,
