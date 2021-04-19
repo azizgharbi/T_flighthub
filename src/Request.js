d5610fcf173d38012071355317cf681c4e0dc42d
aziz <aziz@bunid.ca>
2021-04-19 00:13:58 -0400

    remove unused files

--- src/Request.js
+++ src/Request.js
@@ -1,14 +1,10 @@
 import axios from 'axios';
 
-const Request = async function (url) {
+export default async function (url) {
   try {
-    const instance = axios.create();
-    instance.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-    const { data } = await instance.get(url);
+    const { data } = await axios.get(url);
     return data;
   } catch (err) {
     console.error(err);
   }
 };
-
-export default Request;
